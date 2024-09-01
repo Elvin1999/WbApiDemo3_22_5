4eec1f63d216f68cdf39fe83f7c5a0b5348ba619
camalzadeelvin <camalzadeelvin@STHQ0122-01>
2024-09-01 14:28:21 +0400

    done

--- WbApiDemo3_22_5/Program.cs
+++ WbApiDemo3_22_5/Program.cs
@@ -1,18 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using WbApiDemo3_22_5.Data;
+using WbApiDemo3_22_5.Formatters;
 using WbApiDemo3_22_5.Repository.Abstract;
 using WbApiDemo3_22_5.Repository.Concrete;
+using WbApiDemo3_22_5.Services.Abstract;
+using WbApiDemo3_22_5.Services.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.Insert(0, new VCardOutputFormatter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<IStudentService,StudentService>();
 
 
 var conn = builder.Configuration.GetConnectionString("Default");
