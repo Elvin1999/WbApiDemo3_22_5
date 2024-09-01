7752a6c6dcf438770818f38697fa95ff6071f34b
camalzadeelvin <camalzadeelvin@STHQ0122-01>
2024-09-01 13:27:47 +0400

    done with repos

--- WbApiDemo3_22_5/Program.cs
+++ WbApiDemo3_22_5/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using WbApiDemo3_22_5.Data;
+using WbApiDemo3_22_5.Repository.Abstract;
+using WbApiDemo3_22_5.Repository.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
+
+var conn = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<StudentDbContext>(opt =>
+{
+    opt.UseSqlServer(conn);
+});
 
 var app = builder.Build();
 
